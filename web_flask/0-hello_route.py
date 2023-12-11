54a83486b28bff78f4c0c4cef180e68c4817ba3a
EsmeraldaMar <6580@holbertonstudents.com>
2023-12-11 19:05:14 +1100

    fix: pycodestyle task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,9 +10,12 @@ from flask import Flask
 # creates instance of flask class (WSGI app)
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """ Prints Hello HBNB """
     return 'Hello HBNB!'
 
+
 if __name__ == "__main__":
-      app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
