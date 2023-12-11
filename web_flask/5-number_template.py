9ee9791e751d5985bab8b9225ac5a94cbc7b782a
EsmeraldaMar <6580@holbertonstudents.com>
2023-12-11 20:34:52 +1100

    feat: added template and route for template for task 5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
+    /hbnb: display “HBNB”
+    /c/<text>: display “C ” followed by the value of the text variable
+    /python/<text>: display “Python ”,
+    followed by the value of the text variable
+    default value of text is “is cool”
+    /number/<n>: display “n is a number” only if n is an integer
+    /number_template/<n>: display a HTML page only if n is an integer:
+    H1 tag: “Number: n” inside the tag BODY
+"""
+from flask import Flask, render_template
+# creates instance of flask class (WSGI app)
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Prints Hello HBNB """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Prints Hello HBNB """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_text(text):
+    """ Prints C and <text> """
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py_text(text):
+    """ Prints Python and <text> """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def input_int(n):
+    """ Prints integer"""
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
